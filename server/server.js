bd75bea8902680d5d60972a57403382533c9a252
kolaveridi <coderboysatya@gmail.com>
2018-03-15 03:22:17 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express=require('express');
 var bodyParser=require('body-parser');
-
+var ObjectID = require('mongodb');
+var ObjectID = require('mongodb').ObjectID
 var mongoose=require('./db/mongoose');
 var Todo=require('./models/todo');
 var User=require('./models/user');
@@ -26,6 +27,23 @@ app.get('/todos',(req,res)=>{
     },(e)=>{
       res.status(400).send(e);
     })
+});
+app.get('/todos/:id', (req, res) => {
+  var id=req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 app.listen(3000, () => {
   console.log('Started on port 3000');
